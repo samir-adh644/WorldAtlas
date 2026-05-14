0c0c8fe09a37ea9b3fd271b986da5d59c0728f73
Samir Adhikari <081bct071@ioepc.edu.np>
2026-05-14 16:14:15 +0545

    navbar section

--- src/components/ui/Headers.jsx
+++ src/components/ui/Headers.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Headers() {
   return (
-    <h1>Headers</h1>
+    <header>
+        <div className="container">
+            <div className="grid navbar-grid">
+                <div className="logo">
+                    <NavLink to="/">
+                        <h1>WorldAtlas</h1>
+                    </NavLink>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/">
+                            <a>Home</a>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">
+                            <a>About</a>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/country">
+                            <a>Country</a>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">
+                           <a> Contact</a>
+                            </NavLink>
+                        </li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
   )
 }
 
