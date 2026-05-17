f5a058259ecb0119a3927bcc33d36114b74ed0a6
Samir Adhikari <081bct071@ioepc.edu.np>
2026-05-17 07:17:04 +0545

    footerSection done

--- src/components/ui/Footers.jsx
+++ src/components/ui/Footers.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import footerContact from"../../api/footerApi.json"
+import { MdPlace } from 'react-icons/md';
+import { IoCallSharp } from 'react-icons/io5';
+import { TbMailPlus } from 'react-icons/tb';
 
 function Footers() {
+  const footerIcon = {
+    MdPlace: <MdPlace/>,
+    IoCallSharp:<IoCallSharp/>,
+    TbMailPlus:<TbMailPlus/>
+  }
   return (
-    <h1>Footers</h1>
+    <footer className='footer-section'>
+      <div className="container grid grid-three-cols">
+        {
+          footerContact.map((curData,index)=>{
+            const {icon,title,details}=curData;
+            return(
+              <div className="footer-contact" key={index}>
+                <div className="icon">{footerIcon[icon]}</div>
+                <div className="footer-contact-text">
+                  <p>{title}</p>
+                  <p>{details}</p>
+                </div>
+              </div>
+            );
+          })
+
+        }
+      </div>
+
+    </footer>
   )
 }
 
